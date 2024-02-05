85c8449cd40ecde0b7ab45ca02c29214a7e3ffdc
Sergio Correa <felizjanuk12@gmail.com>
2024-02-05 18:10:24 -0500

    remove dap

--- config/nvim/luaConf/lua/enkdress/plugins.lua
+++ config/nvim/luaConf/lua/enkdress/plugins.lua
@@ -1,83 +1,76 @@
 -- Install package manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
 		lazypath,
-	}
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+require("lazy").setup({
 	{
-		'lewis6991/gitsigns.nvim',
+		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("enkdress.gitsigns").setup()
 		end,
 		event = "BufEnter",
-		cmd = "Gitsigns"
+		cmd = "Gitsigns",
 	},
-	'tpope/vim-fugitive',
-	'tpope/vim-rhubarb',
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
 
-	'windwp/nvim-ts-autotag',
-	'norcalli/nvim-colorizer.lua',
+	"windwp/nvim-ts-autotag",
+	"norcalli/nvim-colorizer.lua",
 	{
 		"nvim-tree/nvim-web-devicons",
 		enabled = true,
 		lazy = true,
 	},
-	'mg979/vim-visual-multi',
+	"mg979/vim-visual-multi",
 
 	-- Detect tabstop and shiftwidth automatically
-	'tpope/vim-sleuth',
+	"tpope/vim-sleuth",
 
 	{
 		-- LSP Configuration & Plugins
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		dependencies = {
 			-- Automatically install LSPs to stdpath for neovim
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
-			'mhartington/formatter.nvim',
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"mhartington/formatter.nvim",
 
 			-- Additional lua configuration, makes nvim stuff amazing!
-			'folke/neodev.nvim',
+			"folke/neodev.nvim",
 		},
 	},
-	{
-		'rcarriga/nvim-dap-ui',
-		dependencies = {
-			'mfussenegger/nvim-dap'
-		},
-	},
-
 	{
 		-- Autocompletion
-		'hrsh7th/nvim-cmp',
+		"hrsh7th/nvim-cmp",
 		dependencies = {
 			-- Snippet Engine & its associated nvim-cmp source
 			{
 				"L3MON4D3/LuaSnip",
 				version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-				build = "make install_jsregexp"
+				build = "make install_jsregexp",
 			},
-			'saadparwaiz1/cmp_luasnip',
+			"saadparwaiz1/cmp_luasnip",
 
 			-- Adds LSP completion capabilities
-			'hrsh7th/cmp-nvim-lsp',
+			"hrsh7th/cmp-nvim-lsp",
 
 			-- Adds a number of user-friendly snippets
-			'rafamadriz/friendly-snippets',
+			"rafamadriz/friendly-snippets",
 		},
 	},
 
 	-- Useful plugin to show you pending keybinds.
-	{ 'folke/which-key.nvim', opts = {} },
+	{ "folke/which-key.nvim", opts = {} },
 	{ "folke/zen-mode.nvim" },
 	-- breadcrumbs
 	{
@@ -89,53 +82,54 @@ require('lazy').setup({
 		enabled = true,
 	},
 
-	{ 'rose-pine/neovim',         name = 'rose-pine' },
-	{ 'navarasu/onedark.nvim' },
-	{ "catppuccin/nvim",          as = "catppuccin" },
-	{ 'sainnhe/gruvbox-material', as = 'gruvbox' },
-	{ 'arcticicestudio/nord-vim', as = 'nord' },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "navarasu/onedark.nvim" },
+	{ "catppuccin/nvim", as = "catppuccin" },
+	{ "sainnhe/gruvbox-material", as = "gruvbox" },
+	{ "arcticicestudio/nord-vim", as = "nord" },
 	{
-		'Enkdress/nordic.nvim',
+		"Enkdress/nordic.nvim",
 		lazy = false,
 		priority = 1000,
 	},
 
-	{ 'nvim-lualine/lualine.nvim' },
+	{ "nvim-lualine/lualine.nvim" },
 	{
-		'lukas-reineke/indent-blankline.nvim',
-		main = 'ibl',
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
 		opts = {},
 	},
 
 	-- "gc" to comment visual regions/lines
-	{ 'numToStr/Comment.nvim',    opts = {} },
+	{ "numToStr/Comment.nvim", opts = {} },
 
 	-- Fuzzy Finder (files, lsp, etc)
 	{
-		'nvim-telescope/telescope.nvim',
-		version = '*',
+		"nvim-telescope/telescope.nvim",
+		version = "*",
 		dependencies = {
-			'nvim-lua/plenary.nvim' }
+			"nvim-lua/plenary.nvim",
+		},
 	},
-	{ 'nvim-telescope/telescope-file-browser.nvim' },
-	{ 'ThePrimeagen/harpoon',                      depedencies = { 'nvim-lua/plenary.nvim' } },
+	{ "nvim-telescope/telescope-file-browser.nvim" },
+	{ "ThePrimeagen/harpoon", depedencies = { "nvim-lua/plenary.nvim" } },
 
 	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build = 'make',
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
 		cond = function()
-			return vim.fn.executable 'make' == 1
+			return vim.fn.executable("make") == 1
 		end,
 	},
 
 	{
 		-- Highlight, edit, and navigate code
-		'nvim-treesitter/nvim-treesitter',
+		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		config = function()
-			pcall(require('nvim-treesitter.install').update { with_sync = true })
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
 		end,
 	},
 	{
@@ -147,15 +141,13 @@ require('lazy').setup({
 				"<leader>ss",
 				mode = { "n", "x", "o" },
 				function()
-					require("flash").jump(
-						{
-							search = {
-								mode = function(str)
-									return "\\<" .. str
-								end,
-							},
-						}
-					)
+					require("flash").jump({
+						search = {
+							mode = function(str)
+								return "\\<" .. str
+							end,
+						},
+					})
 				end,
 				desc = "Flash",
 			},

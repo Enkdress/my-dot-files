9c27eeb9bb5475d96063643e2534b15fca0c9ea3
Sergio Correa <felizjanuk12@gmail.com>
2026-03-13 17:04:41 -0500

    change some nvim things

--- config/nvim/luaConf/lua/custom/plugins/init.lua
+++ config/nvim/luaConf/lua/custom/plugins/init.lua
@@ -1,3 +1,46 @@
 return {
 	"Enkdress/tmux_extension",
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {
+			search = {
+				mode = "search",
+			},
+			highlight = {
+				backdrop = true,
+				matches = true,
+				priority = 5000,
+			},
+			prompt = {
+				enabled = true,
+			},
+		},
+		keys = {
+			{
+				"<leader>fj",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash search",
+			},
+			{
+				"<leader>fjt",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash treesitter",
+			},
+			{
+				"<leader>fjr",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote flash",
+			},
+		},
+	},
 }
--- config/nvim/luaConf/lua/enkdress/plugins.lua
+++ config/nvim/luaConf/lua/enkdress/plugins.lua
@@ -161,40 +161,5 @@ require("lazy").setup({
 			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
 		end,
 	},
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"<leader>ss",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump({
-						search = {
-							mode = "search",
-						},
-					})
-				end,
-				desc = "Flash",
-			},
-			{
-				"<leader>sS",
-				mode = { "n", "o", "x" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter ",
-			},
-			{
-				"<leader>sr",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-		},
-	},
 	{ import = "custom.plugins" },
 }, {})
